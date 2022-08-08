67c2ca62e22455f1b6ecd03838aafd31b9988af3
in63119 <in63119@gmail.com>
2022-08-08 15:08:20 +0900

    solved Plus problem

--- scripts/plusCalculatorSol.js
+++ scripts/plusCalculatorSol.js
@@ -2,23 +2,28 @@ const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 async function calculatorSol() {
-    const [myAccount] = await ethers.getSigners();
-    
-    const MyPlusCalculator = await ethers.getContractFactory("MyPlusCalculator");
-    const myPlusCalculator = await MyPlusCalculator.connect(myAccount).deploy(); 
-    await myPlusCalculator.deployed();
+  const [myAccount] = await ethers.getSigners();
 
-    const instance = "0x000...000"; // 이곳에 덧셈 문제 인스턴스 컨트랙트 주소를 넣으세요
-    const PlusCalculatorProblem = await ethers.getContractFactory("PlusCalculatorProblem");
-    const plusCalculatorProblem = PlusCalculatorProblem.attach(instance);
-    plusCalculatorProblem.connect(myAccount).setPlusCalculator(myPlusCalculator.address);
+  const MyPlusCalculator = await ethers.getContractFactory("MyPlusCalculator");
+  const myPlusCalculator = await MyPlusCalculator.connect(myAccount).deploy();
+  await myPlusCalculator.deployed();
+
+  const instance = "0x08F453aFcA05A4655ca104D7633d88c660De7543"; // 이곳에 덧셈 문제 인스턴스 컨트랙트 주소를 넣으세요
+  const PlusCalculatorProblem = await ethers.getContractFactory(
+    "PlusCalculatorProblem"
+  );
+  const plusCalculatorProblem = PlusCalculatorProblem.attach(instance);
+  const result = await plusCalculatorProblem
+    .connect(myAccount)
+    .setPlusCalculator(myPlusCalculator.address);
+  console.log(result);
 }
 
 async function main() {
-    calculatorSol();
+  calculatorSol();
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
